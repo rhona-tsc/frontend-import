f85b7aacd162e0a993b7c1cd11405082b38e87fb
rhona-tsc <rhona@thesupremecollective.co.uk>
2025-10-16 17:20:06 +0100

    adjusting circle count positio on navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -165,7 +165,7 @@ const Navbar = () => {
           {/* Cart */}
           <Link to="/cart" className="relative p-2 rounded hover:bg-gray-100 active:scale-95">
             <img src={assets.cart_icon} alt="Cart" className="w-5 h-5" />
-            <span className="absolute -right-1.5 -bottom-1.5 min-w-4 px-1 text-center bg-black text-white rounded-full text-[10px] leading-4">
+            <span className="absolute -right-1.5 -bottom-3 min-w-4 px-1 text-center bg-black text-white rounded-full text-[10px] leading-4">
               {getCartCount()}
             </span>
             {cartUpdated && (
