702370f47dd0fdfd281ed8e660220ea6ac35cebf
rhona-tsc <rhona@thesupremecollective.co.uk>
2025-12-07 17:27:41 +0000

    fixed typo

--- src/pages/Acts.jsx
+++ src/pages/Acts.jsx
@@ -1,4 +1,4 @@
-+ import React, { useContext, useState, useEffect, useRef, useMemo } from "react";
+import React, { useContext, useState, useEffect, useRef, useMemo } from "react";
 import { ShopContext } from "../context/ShopContext";
 import { useNavigate } from "react-router-dom";
 
