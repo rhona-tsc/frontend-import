d44129a84ad54060815bb5c7253f71bdec1a1b9d
rhona-tsc <rhona@thesupremecollective.co.uk>
2026-02-10 15:22:38 +0000

    cycled back search bx

--- src/App.jsx
+++ src/App.jsx
@@ -110,11 +110,10 @@ import AuthGateModal from "./components/AuthGateModal";
           
         />
 
-        <SearchBox />
-        <AuthGateModal />
-
         <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[4vw]">
           <Navbar />
+          <SearchBox />
+          <AuthGateModal />
           <div className="mt-16">
             <Routes>
               <Route path="/" element={<Home />} />
